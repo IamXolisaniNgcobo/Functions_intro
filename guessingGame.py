ab32075e342233d3a74f8a8bf10e667b430a668c
XOLISANI NGCOBO <38984822+IamXolisaniNgcobo@users.noreply.github.com>
2021-06-20 21:44:37 +0200

    Update guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -2,6 +2,16 @@ import random
 
 
 def get_integer(prompt):
+    """
+    Get an integer from Standard Input (stdin).
+
+    The function will continue looping, and prompting the user until
+    valid `int` is entered.
+
+    :param prompt: The String that the user will see, when
+        they're prompted to enter the value.
+    :return:The integer that the user enters.
+    """
     while True:
         temp = input(prompt)
         if temp.isnumeric():
@@ -10,6 +20,12 @@ def get_integer(prompt):
             print('{0} is not a valid number! try again'.format(temp))
 
 
+print(input.__doc__)
+print("*" * 80)
+help(get_integer)
+print("*" * 80)
+print(get_integer.__doc__)
+print('*' * 80)
 highest = 1000
 answer = random.randint(1, highest)
 print(answer)  # TODO: remove after testing
